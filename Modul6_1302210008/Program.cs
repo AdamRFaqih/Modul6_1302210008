fa6d0a8048abf47ef03b7ace44979195e87f0c4a
Chandra Bayu Samudra <kmalware88@gmail.com>
2023-03-28 15:37:48 +0700

    first commit

---
+++ Modul6_1302210008/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Modul6_1302210008
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
